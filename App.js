9a67e2f5b58d59b3b32145188bcd5db1773fd9a8
Jtan <jtan0895@gmail.com>
2022-07-02 15:06:05 +0800

    Added List View

--- App.js
+++ App.js
@@ -1,19 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
+
+  function NotesScreen({ navigation }) {
+    const [notes, setNotes] = useState([
+      { title: "Walk the cat", done: false, id: "0" },
+      { title: "Feed the elephant", done: false, id: "1" },
+    ]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => 
+      <TouchableOpacity onPress={addNote} style={styles.headerIcon}>
+      <Entypo 
+      name="new-message" 
+      size={24} 
+      color="black" 
+      style={{ marginRight: 20 }} 
+      />
+    </TouchableOpacity>
+    });
+  });
+
+  function addNote() {
+    console.log("Add Note");
+    let newNote = {
+      title: "Sample new note",
+      done: false,
+      id: notes.length.toString(),
+    };
+    setNotes([...notes, newNote]);
+  }
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+ 
+
+  return (<View style={styles.container}>
+     <FlatList
+       style={{ width: "100%" }}
+       data={notes}
+       renderItem={renderItem}
+     />
+  </View>);
+ }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Notes" component={NotesScreen}
+                options={{
+                  headerTitle: "Notes App",
+                  headerTitleStyle: {
+                    fontWeight: "bold",
+                    fontSize: 30,
+                  },
+                  headerStyle: {
+                    height: 120,
+                    backgroundColor: "yellow",
+                    borderBottomColor: "#ccc",
+                    borderBottomWidth: 1,
+                  },
+                }}
+       />
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc',
     alignItems: 'center',
     justifyContent: 'center',
   },
