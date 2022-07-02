699de7b5949df5a0dc83d51c9587545eba177f48
Jtan <jtan0895@gmail.com>
2022-07-02 15:53:46 +0800

    New Add Todo

--- App.js
+++ App.js
@@ -1,93 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
 import * as SQLite from "expo-sqlite";
+import NotesStack from "./Screens/NotesStack";
+import AddScreen from "./Screens/AddScreen";
 
 const db = SQLite.openDatabase("notes.db");
-
-  function NotesScreen({ navigation }) {
-    const [notes, setNotes] = useState([
-      { title: "Walk the cat", done: false, id: "0" },
-      { title: "Feed the elephant", done: false, id: "1" },
-    ]);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => 
-      <TouchableOpacity onPress={addNote} style={styles.headerIcon}>
-      <Entypo 
-      name="new-message" 
-      size={24} 
-      color="black" 
-      style={{ marginRight: 20 }} 
-      />
-    </TouchableOpacity>
-    });
-  });
-
-  function addNote() {
-    console.log("Add Note");
-    let newNote = {
-      title: "Sample new note",
-      done: false,
-      id: notes.length.toString(),
-    };
-    setNotes([...notes, newNote]);
-  }
-  function renderItem({ item }) {
-    return (
-      <View
-        style={{
-          padding: 10,
-          paddingTop: 20,
-          paddingBottom: 20,
-          borderBottomColor: "#ccc",
-          borderBottomWidth: 1,
-        }}
-      >
-        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
-      </View>
-    );
-  }
- 
-
-  return (<View style={styles.container}>
-     <FlatList
-       style={{ width: "100%" }}
-       data={notes}
-       renderItem={renderItem}
-     />
-  </View>);
- }
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Notes" component={NotesScreen}
-                options={{
-                  headerTitle: "Notes App",
-                  headerTitleStyle: {
-                    fontWeight: "bold",
-                    fontSize: 30,
-                  },
-                  headerStyle: {
-                    height: 120,
-                    backgroundColor: "yellow",
-                    borderBottomColor: "#ccc",
-                    borderBottomWidth: 1,
-                  },
-                }}
-       />
-     </Stack.Navigator>
-   </NavigationContainer>
+      <Stack.Navigator mode="modal" headerMode="none">
+        <Stack.Screen
+          name="Notes Stack"
+          component={NotesStack}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Add Note" component={AddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+ }
+ 
 
 const styles = StyleSheet.create({
   container: {
