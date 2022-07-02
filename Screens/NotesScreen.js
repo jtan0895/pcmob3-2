524f5db95e417dcd0d1c46a1c746a259e3ddac53
Jtan <jtan0895@gmail.com>
2022-07-02 19:12:42 +0800

    Update with SQLLite

--- Screens/NotesScreen.js
+++ Screens/NotesScreen.js
@@ -7,51 +7,107 @@ import {
  TouchableOpacity,
 } from "react-native";
 import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
 
 export default function NotesScreen({ navigation, route }) {
  const [notes, setNotes] = useState([
-   { title: "Eat Medicine", done: false, id: "0" },
+  {/*
+    { title: "Eat Medicine", done: false, id: "0" },
    { title: "Sleep", done: false, id: "1" },
+ */}
  ]);
 
- useEffect(() => {
-   navigation.setOptions({
-     headerRight: () => (
-       <TouchableOpacity onPress={addNote}>
-         <Entypo
-           name="new-message"
-           size={24}
-           color="black"
-           style={{ marginRight: 20 }}
-         />
-       </TouchableOpacity>
-     ),
-   });
- });
+ function refreshNotes() {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM notes",
+        null,
+        (txObj, { rows: { _array } }) => setNotes(_array),
+        (txObj, error) => console.log(`Error: ${error}`)
+      );
+    });
+  }
+
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          `CREATE TABLE IF NOT EXISTS notes
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          done INT)
+        `
+        );
+      },
+      null,
+      refreshNotes
+    );
+  }, []);
 
  useEffect(() => {
+    navigation.setOptions({
+        headerRight: () => (
+          <TouchableOpacity onPress={addNote}>
+            <Entypo
+              name="new-message"
+              size={24}
+              color="black"
+              style={{ marginRight: 20 }}
+            />
+          </TouchableOpacity>
+        ),
+      });
+    });
 
-    //If route.params is not Falsy
+    // Monitor route.params for changes and add items to the database
+  useEffect(() => {
     if (route.params?.text) {
-      //db.transaction((tx) => {
-      //  tx.executeSql("INSERT INTO notes (done, value) VALUES (0, ?)", [
-      //    route.params.text,
-      //  ]);
-      //});
+      db.transaction(
+        (tx) => {
+          tx.executeSql("INSERT INTO notes (done, title) VALUES (0, ?)", 
+          [
+            route.params.text,
+          ]);
+        },
+        null,
+        refreshNotes
+      );
+    }
+  }, [route.params?.text]);
  
+{/*
+    useEffect(() => {
+    if (route.params?.text) {
+      
       const newNote = {
         title: route.params.text,
         done: false,
         id: notes.length.toString(),
       };
       setNotes([...notes, newNote]);
+    
     }
   }, [route.params?.text]);
- 
+*/}
+
 
  function addNote() {
    navigation.navigate("Add Note");
  }
+
+ // This deletes an individual note
+ function deleteNote(id) {
+    console.log("Deleting " + id);
+    db.transaction(
+      (tx) => {
+        tx.executeSql(`DELETE FROM notes WHERE id=${id}`);
+      },
+      null,
+      refreshNotes
+    );
+  }
 
  function renderItem({ item }) {
    return (
@@ -62,9 +118,17 @@ export default function NotesScreen({ navigation, route }) {
          paddingBottom: 20,
          borderBottomColor: "#ccc",
          borderBottomWidth: 1,
+         flexDirection: "row",
+         justifyContent: "space-between",
        }}
      >
        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+       <TouchableOpacity onPress={() => deleteNote(item.id)}>
+          <Entypo
+          name="trash" 
+          size={16} 
+          color="#944" />
+        </TouchableOpacity>
      </View>
    );
  }
@@ -75,6 +139,7 @@ export default function NotesScreen({ navigation, route }) {
        style={{ width: "100%" }}
        data={notes}
        renderItem={renderItem}
+       
      />
    </View>
  );
